ac4597e97511223adc66c43816effdbd5b11d3c3
Sam <s-peeters@hotmail.com>
2022-08-16 16:16:05 +0200

    Initial commit

---
+++ Peeters_Sam_r049890/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Peeters_Sam_r049890.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
