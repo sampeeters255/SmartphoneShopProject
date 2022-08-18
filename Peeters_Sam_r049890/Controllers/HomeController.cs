86ded7ae66d07edd39e01807e36046051834aef4
Sam <s-peeters@hotmail.com>
2022-08-18 21:33:38 +0200

    changed home and navbar

--- Peeters_Sam_r049890/Controllers/HomeController.cs
+++ Peeters_Sam_r049890/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Peeters_Sam_r049890.Data;
 using Peeters_Sam_r049890.Models;
 using System;
 using System.Collections.Generic;
@@ -12,18 +14,22 @@ namespace Peeters_Sam_r049890.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+    
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
-        }
+      _context = context;
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public async Task<IActionResult> Index()
+    {
+      var data = await _context.Smartphones.ToListAsync();
+      return View(data);
+    }
 
-        public IActionResult Privacy()
+    public IActionResult Privacy()
         {
             return View();
         }
